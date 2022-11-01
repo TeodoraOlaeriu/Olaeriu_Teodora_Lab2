9828d4d2f0fad694455e7dc905d84d8fe0835813
Teo <teodoraolaeriu@yahoo.com>
2022-11-01 17:18:09 +0200

    Laborator4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,12 @@
+﻿using Olaeriu_Teodora_Lab2.Models;
+
+namespace Olaeriu_Teodora_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
